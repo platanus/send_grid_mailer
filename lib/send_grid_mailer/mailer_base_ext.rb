657a1f5aaed769d5ee21d359f0452d05e42fcb7b
Leandro Segovia <ldlsegovia@gmail.com>
2016-09-25 16:30:44 -0300

    feat(ActionMailer::Base): override mail method to add empty body in order to avoid missing template error

--- lib/send_grid_mailer/mailer_base_ext.rb
+++ lib/send_grid_mailer/mailer_base_ext.rb
@@ -1,9 +1,17 @@
 module ActionMailer
   class Base < AbstractController::Base
+    alias_method :old_mail, :mail
+
     Mail::Message::SG_METHODS.each do |method_name|
       define_method(method_name) do |*args|
         @_message.send(method_name, *args)
       end
+    end
+
+    def mail(headers = {}, &block)
+      # Adding empty body with sendgrid delivery method to avoid template missing exception.
+      headers[:body] = "" if self.class.delivery_method == :sendgrid
+      old_mail(headers, &block)
     end
   end
 end
