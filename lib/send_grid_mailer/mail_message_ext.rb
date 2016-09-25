35ef86618f78e96fa2c8ae906d156c4722897ddc
Leandro Segovia <ldlsegovia@gmail.com>
2016-09-25 12:20:31 -0300

    feat(Mail::Message): add substitutions

--- lib/send_grid_mailer/mail_message_ext.rb
+++ lib/send_grid_mailer/mail_message_ext.rb
@@ -1,5 +1,15 @@
 module Mail
   class Message
     attr_accessor :template_id
+
+    def substitute(key, value)
+      personalization.substitutions = SendGrid::Substitution.new(key: key, value: value)
+    end
+
+    private
+
+    def personalization
+      @personalization ||= SendGrid::Personalization.new
+    end
   end
 end
--- spec/dummy/spec/lib/send_grid_mailer/mail_message_ext_spec.rb
+++ spec/dummy/spec/lib/send_grid_mailer/mail_message_ext_spec.rb
@@ -1,8 +1,31 @@
 require "rails_helper"
 
 describe Mail::Message do
-  it "has template_id attr_accessor" do
-    subject.template_id = "X"
-    expect(subject.template_id).to eq("X")
+  describe "#template_id" do
+    it "has template_id attr_accessor" do
+      subject.template_id = "X"
+      expect(subject.template_id).to eq("X")
+    end
+  end
+
+  let(:personalization) { subject.send(:personalization) }
+
+  describe "#substitute" do
+    before do
+      @substitution = subject.substitute("%subject%", "Hi!")
+    end
+
+    it "creates substitution with valid data" do
+      expect(@substitution.substitution).to eq("%subject%" => "Hi!")
+    end
+
+    it "adds substitution to personalization object" do
+      expect(personalization.substitutions.size).to eq(1)
+    end
+
+    it "adds substitution to collection" do
+      subject.substitute("%body%", "blah")
+      expect(personalization.substitutions.size).to eq(2)
+    end
   end
 end
