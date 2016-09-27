08ff44057fb9913f183f2a72a89d2cbb9884df16
Leandro Segovia <ldlsegovia@gmail.com>
2016-09-27 15:49:59 -0300

    fix(): require files on initialize

--- lib/send_grid_mailer/engine.rb
+++ lib/send_grid_mailer/engine.rb
@@ -1,10 +1,3 @@
-require_relative "./errors"
-require_relative "./logger"
-require_relative "./definition"
-require_relative "./mail_message_ext"
-require_relative "./mailer_base_ext"
-require_relative "./deliverer"
-
 module SendGridMailer
   class Engine < ::Rails::Engine
     isolate_namespace SendGridMailer
@@ -14,7 +7,16 @@ module SendGridMailer
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 
+    initializer "initialize" do
+      require_relative "./errors"
+      require_relative "./logger"
+      require_relative "./definition"
+      require_relative "./mail_message_ext"
+      require_relative "./mailer_base_ext"
+    end
+
     initializer "add_sendgrid_deliverer", before: "action_mailer.set_configs" do
+      require_relative "./deliverer"
       ActionMailer::Base.add_delivery_method(:sendgrid, SendGridMailer::Deliverer)
     end
   end
