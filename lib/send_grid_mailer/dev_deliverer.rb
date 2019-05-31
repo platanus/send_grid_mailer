ec1d371015f52fd5ecde957c03e3d068daebfd45
Diego Fernández <difernandez@uc.cl>
2019-05-31 11:17:24 -0400

    feat(dev-deliverer): use letter opener to open sg template in browser

--- lib/send_grid_mailer/dev_deliverer.rb
+++ lib/send_grid_mailer/dev_deliverer.rb
@@ -2,15 +2,14 @@ module SendGridMailer
   class DevDeliverer
     include InterceptorsHandler
     include Logger
+    require "letter_opener"
 
     def deliver!(sg_definition)
       execute_interceptors(sg_definition)
-      response = sg_api.get_template(sg_definition)
-      template_active_version = JSON.parse(response.body)["versions"].find do |version|
-        version["active"] == 1
+      mail = Mail.new do |m|
+        m.html_part = parsed_template(sg_definition).html_safe
       end
-      template_content = template_active_version["html_content"]
-      log_template(template_content, sg_definition.personalization.substitutions)
+      letter_opener_delivery_method.deliver!(mail)
     end
 
     private
@@ -23,6 +22,20 @@ module SendGridMailer
       Rails.application.config.action_mailer.sendgrid_dev_settings[:api_key]
     rescue
       nil
+    end
+
+    def letter_opener_delivery_method
+      @letter_opener_delivery_method ||= LetterOpener::DeliveryMethod.new(location: '/tmp/mails')
+    end
+
+    def parsed_template(sg_definition)
+      template_response = sg_api.get_template(sg_definition)
+      template_active_version = JSON.parse(template_response.body)["versions"].find do |version|
+        version["active"] == 1
+      end
+      template_content = template_active_version["html_content"]
+      sg_definition.personalization.substitutions.each { |k, v| template_content.gsub!(k, v) }
+      template_content
     end
   end
 end
