25c8cad977a62654a2e80bcb88b8fc2d33b57958
Diego Fernández <difernandez@uc.cl>
2019-05-31 12:39:57 -0400

    refactor(dev-deliverer): avoid passing sg_definition inside dev_deliverer

--- lib/send_grid_mailer/dev_deliverer.rb
+++ lib/send_grid_mailer/dev_deliverer.rb
@@ -5,9 +5,10 @@ module SendGridMailer
     require "letter_opener"
 
     def deliver!(sg_definition)
-      execute_interceptors(sg_definition)
-      log_definition(sg_definition)
-      letter_opener_delivery_method.deliver!(mail(sg_definition))
+      @sg_definition = sg_definition
+      execute_interceptors(@sg_definition)
+      log_definition(@sg_definition)
+      letter_opener_delivery_method.deliver!(mail)
     end
 
     private
@@ -26,31 +27,31 @@ module SendGridMailer
       @letter_opener_delivery_method ||= LetterOpener::DeliveryMethod.new(location: '/tmp/mails')
     end
 
-    def parsed_template(sg_definition)
-      template_response = sg_api.get_template(sg_definition)
+    def parsed_template
+      template_response = sg_api.get_template(@sg_definition)
       template_active_version = JSON.parse(template_response.body)["versions"].find do |version|
         version["active"] == 1
       end
       template_content = template_active_version["html_content"]
-      sg_definition.personalization.substitutions.each { |k, v| template_content.gsub!(k, v) }
+      @sg_definition.personalization.substitutions.each { |k, v| template_content.gsub!(k, v) }
       template_content
     end
 
-    def emails(sg_definition, origin)
+    def emails(origin)
       @emails ||= {}
       return @emails[origin] if @emails.has_key?(origin)
 
-      @emails[origin] = sg_definition.personalization.send(origin)&.map {|em| em["email"]}
+      @emails[origin] = @sg_definition.personalization.send(origin)&.map {|em| em["email"]}
     end
 
-    def mail(sg_definition)
+    def mail
       Mail.new do |m|
-        m.html_part = parsed_template(sg_definition).html_safe
-        m.subject = sg_definition.personalization.subject
-        m.from = sg_definition.mail.from["email"] if sg_definition.mail.from.present?
-        m.to = emails(sg_definition, :tos) if emails(sg_definition, :tos).present?
-        m.cc = emails(sg_definition, :ccs) if emails(sg_definition, :ccs).present?
-        m.bcc = emails(sg_definition, :bccs) if emails(sg_definition, :bccs).present?
+        m.html_part = parsed_template.html_safe
+        m.subject = @sg_definition.personalization.subject
+        m.from = @sg_definition.mail.from["email"] if @sg_definition.mail.from.present?
+        m.to = emails(:tos) if emails(:tos).present?
+        m.cc = emails(:ccs) if emails(:ccs).present?
+        m.bcc = emails(:bccs) if emails(:bccs).present?
       end
     end
   end
